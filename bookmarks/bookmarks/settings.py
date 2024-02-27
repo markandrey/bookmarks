a50fd73b5728aeffc345910007d8e5edace810ca
Mark Andrey <markandreym@gmail.com>
2024-02-27 21:12:43 +0300

    e-mail auth

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -135,3 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # закачивание файлов на сайт и раздача медиафайлов
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
